7d30fb2d2a319c26f5d09a0d13e7dde8a3d4cec2
antonkostiuchkov <antonkostiuchkov@gmail.com>
2016-08-29 18:08:52 +0200

    Label CRUD

--- src/components/label-component.js
+++ src/components/label-component.js
@@ -6,28 +6,76 @@ export default React.createClass({
 
   displayName: 'Label',
 
+  getInitialState () {
+    const {name, color} = this.props.label
+
+    return {name, color}
+  },
+
   onEditClick (event) {
     event.preventDefault()
     this.props.label.editing = true
   },
 
+  onSubmit (event) {
+    event.preventDefault()
+    const {label} = this.props
+
+    if (label.saved) {
+      label.update(this.state)
+    } else {
+      label.save(this.state, {
+        success: () => {
+          label.saved = true
+        }
+      })
+    }
+
+    label.editing = false
+  },
+
   onCancelClick (event) {
     event.preventDefault()
-    this.props.label.editing = false
+    const {label} = this.props
+
+    if (label.saved) {
+      label.editing = false
+      this.setState(this.getInitialState())
+    } else {
+      label.destroy()
+    }
+  },
+
+  onDeleteClick (event) {
+    event.preventDefault()
+    this.props.label.destroy()
+  },
+
+  onNameChange (event) {
+    this.setState({
+      name: event.target.value
+    })
+  },
+
+  onColorChange (event) {
+    this.setState({
+      color: event.target.value.slice(1)
+    })
   },
 
   render () {
     const {label} = this.props
-    const cssColor = '#' + label.color
+    const {color} = this.state
+    const cssColor = '#' + color
     let content
 
     if (label.editing) {
       content = (
-        <form className='label-view'>
-          <span className='label-color avatar avatar-small avatar-rounded'>&nbsp;</span>
-          <input name='name' />
-          <input name='color' />
-          <button type='submit' className='button button-small'>Save</button>
+        <form onSubmit={this.onSubmit} className='label-view'>
+          <span style={{backgroundColor: cssColor}} className='label-color avatar avatar-small avatar-rounded'>&nbsp;</span>
+          <input name='name' onChange={this.onNameChange} value={this.state.name} />
+          <input name='color' onChange={this.onColorChange} value={cssColor} />
+          <button onClick={this.onSubmit} type='submit' className='button button-small'>Save</button>
           <button onClick={this.onCancelClick} type='button' className='button button-small button-unstyled'>cancel</button>
         </form>
       )
@@ -39,7 +87,7 @@ export default React.createClass({
           <a href='' onClick={this.onEditClick}>
             <span className='octicon octicon-pencil' />
           </a>
-          <a href=''>
+          <a href='' onClick={this.onDeleteClick}>
             <span className='octicon octicon-x' />
           </a>
         </div>
--- src/pages/repo-detail.js
+++ src/pages/repo-detail.js
@@ -7,12 +7,24 @@ export default React.createClass({
 
   displayName: 'RepoDetail',
 
+  onAddClick () {
+    this.props.labels.add({
+      name: '',
+      color: '',
+      editing: true,
+      saved: false
+    }, {at: 0})
+  },
+
   render () {
     const {repo, labels} = this.props
 
     return (
       <div className='container'>
         <h1>{repo.full_name}</h1>
+        <div>
+          <button onClick={this.onAddClick} className='button'>Add New</button>
+        </div>
         <ul>
           {labels.map(label => <Label key={label.name} label={label} />)}
         </ul>
